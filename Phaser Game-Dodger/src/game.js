a8641927117d36eb50c2421dc5d1d96c297dbcbd
angelebeard <angelebeard@gmail.com>
2026-07-27 21:04:53 -0500

    Delete Phaser Game-Dodger directory

--- Phaser Game-Dodger/src/game.js
+++
@@ -1,8 +0,0 @@
-const config = new Object(); 
-
-config.width = 640;                                              
-config.height  = 480;    
-config.scene   = [ PlayScene ];                                    //Scenes for this game 
-config.physics = { default:'arcade' };
-                                          
-const game = new Phaser.Game(config);                                
